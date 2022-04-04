5036708027210e25f0d7e5d2a331635361e49a3a
sudha-murthy-pps-2 <102472130+sudha-murthy-pps-2@users.noreply.github.com>
2022-04-04 11:06:26 +0530

    Add files via upload

---
+++ structures.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+struct students{
+int roll ;
+char name[10];
+};
+void main(){
+struct students s;
+s.roll=1234;
+strcpy (s.name,"ramesh");
+printf("roll = %d,name= %s ",roll,name);
+}
